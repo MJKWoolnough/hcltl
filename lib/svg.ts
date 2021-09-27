29dfee7a59c4a82e618042b5bee133aa6265d175
Michael Woolnough <michael.woolnough@gmail.com>
2021-09-27 08:04:50 +0100

    replaced with links to lib

--- lib/svg.ts
+++ lib/svg.ts
@@ -1,6 +1,1 @@
-import type {DOMBind} from './dom.js';
-import {createSVG} from './dom.js';
-
-export {createSVG};
-
-export const [circle, defs, g, line, path, pattern, rect, svg, text, use] = "circle defs g line path pattern rect svg text use".split(" ").map(e => createSVG.bind(null, e)) as [DOMBind<SVGElementTagNameMap["circle"]>,DOMBind<SVGElementTagNameMap["defs"]>,DOMBind<SVGElementTagNameMap["g"]>,DOMBind<SVGElementTagNameMap["line"]>,DOMBind<SVGElementTagNameMap["path"]>,DOMBind<SVGElementTagNameMap["pattern"]>,DOMBind<SVGElementTagNameMap["rect"]>,DOMBind<SVGElementTagNameMap["svg"]>,DOMBind<SVGElementTagNameMap["text"]>,DOMBind<SVGElementTagNameMap["use"]>];
+/home/michael/Programming/Go/src/vimagination.zapto.org/jslib/lib.ts/svg.ts
